5953e13fa6d3c8329cffb6506069a6118e8afee0
Matt <mpickle86@gmail.com>
2021-04-24 14:56:33 -0500

    Initial Commit

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+
+exports.sendEmail = functions.https.onRequest((req, res) => {
+  console.log(req.body.name);
+  const smtpTrans = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: functions.config().gmail.user,
+      pass: functions.config().gmail.pass
+    }
+  });
+  const mailOpts = {
+    from: "",
+    to: functions.config().gmail.user,
+    subject: "From portfolio site contact form",
+    text: `Name: ${req.body.name} \n
+          Company: ${req.body.company} \n
+          Email: ${req.body.email} \n
+          ${req.body.message}`
+  }
+  smtpTrans.sendMail(mailOpts, (err, response) => {
+    if (err) {
+      console.error(err);
+      res.send("Server Error. Please try again or send email to mattpickle@mattpickle.net");
+    } else {
+      res.send("Message sent successfully.");
+    }
+  })
+})
